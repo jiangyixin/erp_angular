86b8949539864ad6f4b0aa5e57158176e3c6303d
jiangyixin <540872052@qq.com>
2016-03-23 15:07:43 +0800

    create

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,32 @@
+<?php if (!defined('THINK_PATH')) exit();?><!doctype html>
+<html ng-app>
+<head>
+    <link rel="stylesheet" href="/php/erp/test_angular/Public/bootstrap-3.1.1/css/bootstrap.css">
+    <script src="/php/erp/test_angular/Public/angular-1.5.0/angular.js"></script>
+</head>
+<body>
+<!--Your name: <input type="text" ng-model="yourname" placeholder="World">
+<hr>
+Hello {{yourname || 'World'}}!-->
+
+<p>Nothing here {{'yet' + '!'}}</p>
+
+<p>1 + 2 = {{ 1 + 2 }}</p>
+
+<ul>
+    <li>
+        <span>Nexus S</span>
+        <p>
+            Fast just got faster with Nexus S.
+        </p>
+    </li>
+    <li>
+        <span>Motorola XOOM™ with Wi-Fi</span>
+        <p>
+            The Next, Next Generation tablet.
+        </p>
+    </li>
+</ul>
+
+</body>
+</html>
